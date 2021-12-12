defac81a08e0aa2f634d446b3d19d86493b1bbd0
unknown <hazem.hassan.11.hh@gmail.com>
2021-12-12 16:29:43 +0200

    mysql lib

--- src/airlinereservationsystems/AirlineReservationSystems.java
+++ src/airlinereservationsystems/AirlineReservationSystems.java
@@ -1,6 +1,5 @@
 package airlinereservationsystems;
 public class AirlineReservationSystems {
     public static void main(String[] args) {
-        System.out.println("airlinereservationsystems.AirlineReservationSystems.main()");
     }
 }
