fe3ce324e8dc149b4cb34abf6d6a0891bcb0382d
unknown <hazem.hassan.11.hh@gmail.com>
2021-12-12 16:11:09 +0200

    project

---
+++ src/airlinereservationsystems/AirlineReservationSystems.java
@@ -0,0 +1,6 @@
+package airlinereservationsystems;
+public class AirlineReservationSystems {
+    public static void main(String[] args) {
+        System.out.println("airlinereservationsystems.AirlineReservationSystems.main()");
+    }
+}
