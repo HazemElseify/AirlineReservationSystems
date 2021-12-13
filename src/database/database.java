09695c702368302b8c6aa50ec27bbf6dc723ff2d
unknown <hazem.hassan.11.hh@gmail.com>
2021-12-13 13:25:51 +0200

    database connection function

--- src/database/database.java
+++ src/database/database.java
@@ -3,11 +3,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package database;
-
+import java.sql.*;
 /**
  *
  * @author Hazem Elseify
  */
 public class database {
-    
+   public final String username="root";
+   public final String password="1234";
+   public final String url="jdbc:mysql://localhost:3306/ars";
+   
+   public static 
 }
