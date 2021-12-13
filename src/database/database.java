791b5ead2643e9188a86e37e33a31cbfa64e723a
unknown <hazem.hassan.11.hh@gmail.com>
2021-12-13 14:22:52 +0200

    database update

--- src/database/database.java
+++ src/database/database.java
@@ -4,14 +4,30 @@
  */
 package database;
 import java.sql.*;
+import java.util.ArrayList;
 /**
  *
  * @author Hazem Elseify
  */
 public class database {
-   public final String username="root";
-   public final String password="1234";
-   public final String url="jdbc:mysql://localhost:3306/ars";
+   public static String username="root";
+   public static String password="1234";
+   public static String url="jdbc:mysql://localhost:3306/ars";
+   public static String planeid;
    
-   public static 
+   public static Connection connect() throws SQLException{
+        Connection con=DriverManager.getConnection(url,username,password);
+       return con;
+   }
+   public static ArrayList<String>getSeatslist(ArrayList<String> arrayList) throws SQLException{
+           Connection con =connect();
+           PreparedStatement statement=con.prepareStatement("SELECT seatno FROM seats WHERE planeid = ?");
+           statement.setString(0, planeid);
+           ResultSet resultSet=statement.executeQuery();
+           while (resultSet.next()) {
+               arrayList.add(resultSet.getString("seatno"));
+          }
+           return arrayList;
+      }
+   
 }
